1b5c077cd1f18f70ce756fe8565ebe309a8687cc
Maksym Makar <wtopm@list.ru>
2023-07-24 16:37:10 +0100

    Added Authentication method

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WTOPMDb.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+using System.Text;
 
 using WTOPMDb.Data;
 
@@ -8,13 +13,28 @@ namespace WTOPMDb
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false).Build();
             var builder = WebApplication.CreateBuilder(args);
-            var connection = @"Data Source=WTOPMPC\SQLEXPRESS;Initial Catalog=WTOPMData;Integrated Security=True;Pooling=False;Encrypt=False";
+            //var connection = @"Data Source=WTOPMPC\SQLEXPRESS;Initial Catalog=WTOPMData;Integrated Security=True;Pooling=False;Encrypt=False";
+            
             // Add services to the container.
 
-            builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(connection));
+           // builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(config.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllersWithViews();
             builder.Services.AddCors();
+            builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(config.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
             var app = builder.Build();
 
@@ -22,11 +42,10 @@ namespace WTOPMDb
             if (!app.Environment.IsDevelopment())
             {
             }
-
             app.UseStaticFiles();
             app.UseRouting();
             app.UseCors(x => x.WithOrigins("http://localhost:44487").AllowCredentials().AllowAnyMethod().AllowAnyHeader());
-
+            app.UseAuthentication();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller}/{action=Index}/{id?}");
