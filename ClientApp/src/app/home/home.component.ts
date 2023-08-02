b791934c42a1da0ad4565f57a664b2eefff62aea
Maksym Makar <wtopm@list.ru>
2023-08-02 21:23:01 +0100

    NavBar Added

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+      this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("http://localhost:5091/users").subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
 }
